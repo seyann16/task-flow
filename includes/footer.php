7ddb56de5b75fb30ad2392cfe6f003d57d95734d
seyann16 <mabbicgwapo@gmail.com>
2025-10-07 18:57:32 +0800

    footer for the web page

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+    </main>
+    <footer class="footer">
+        <div class="footer-content">
+            <p>&copy; <?php echo date('Y'); ?> TaskFlow. All rights reserved.</p>
+        </div>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+</html>
