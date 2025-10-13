272f6c8d769f0e503d252668d7d773bc60645d6b
seyann16 <mabbicgwapo@gmail.com>
2025-10-13 15:03:38 +0800

    imported neccessary js files

--- includes/footer.php
+++ includes/footer.php
@@ -4,6 +4,14 @@
             <p>&copy; <?php echo date('Y'); ?> TaskFlow. All rights reserved.</p>
         </div>
     </footer>
+    <script src="js/utils.js"></script>
+    <script src="js/api.js"></script>
+    <script src="js/tasks.js"></script>
+    <script src="js/ui.js"></script>
+    <script src="js/theme.js"></script>
+    <script src="js/filters.js"></script>
+    <script src="js/modals.js"></script>
+    <script src="js/flash.js"></script>
     <script src="js/core.js"></script>
 </body>
 </html>
